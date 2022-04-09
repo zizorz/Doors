fd26c96351bd43dbe577fce12d68b68d50df59d0
zizorz <zizorz>
2022-04-09 08:41:08 +0200

    Add DoorBuilder

--- Doors/Door.cs
+++ Doors/Door.cs
@@ -1,33 +1,71 @@
-﻿namespace Doors
+﻿using System;
+using System.Collections.Generic;
+
+namespace Doors
 {
     public class Door : IDoor
     {
         private bool _isOpen;
 
-        public string LockType { get; set; }
-        public IDoorBehaviour? OpenBehaviour { get; set; }
-        public IDoorBehaviour? CloseBehaviour { get; set; }
+        public IList<Action> OnOpenActions { get; }
+        public IList<Action> OnCloseActions { get; }
 
+        private Door()
+        {
+            OnOpenActions = new List<Action>();
+            OnCloseActions = new List<Action>();
+        }
 
         public void Open()
         {
             if (_isOpen) { return; }
-
             _isOpen = true;
-            OpenBehaviour?.Run(this);
+            foreach (var action in OnOpenActions)
+            {
+                action();
+            }
         }
 
         public void Close()
         {
             if (!_isOpen) { return; }
-
             _isOpen = false;
-            CloseBehaviour?.Run(this);
+            foreach (var action in OnCloseActions)
+            {
+                action();
+            }
         }
 
         public bool IsOpen()
         {
             return _isOpen;
+        }
+
+        public class DoorBuilder
+        {
+            private readonly Door _door;
+            public DoorBuilder()
+            {
+                _door = new Door();
+            }
+
+            public DoorBuilder WithOpenAction(Action openAction)
+            {
+                _door.OnOpenActions.Add(openAction);
+                return this;
+            }
+
+            public DoorBuilder WithCloseAction(Action closeAction)
+            {
+                _door.OnCloseActions.Add(closeAction);
+                return this;
+            }
+
+            public Door Build()
+            {
+                return _door;
+            }
+
         }
     }
 }
